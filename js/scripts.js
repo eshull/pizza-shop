a98f979f4a81acc2e41bd4252fc71456aa9ac4a2
Eric Shull <eshulldesign@gmail.com>
2018-06-15 13:49:00 -0700

    add function to each topping to get rid of undefined display

--- js/scripts.js
+++ js/scripts.js
@@ -20,38 +20,46 @@ Toppings.prototype.customersToppings = function() {
     return this.topping1 + ", " + this.topping2 + ", " + this.topping3 + ", " + this.topping4;
 }
 
-function checkIfUndefined(one, two, three, four) {
-  toppingsChecked = [];
-  if (one === undefined) {
-    one = "";
-  } else if (two === undefined) {
-    two = "";
-  } else if (three === undefined) {
-    three = "";
-  } else if (four === undefined) {
-    four = "";
+function checkIfUndefined(one) {
+    // toppingsChecked = [];
+    if (one === undefined) {
+      one = "";
+      return one;
+    } else {
+        return one;
+      }
+    // } else if (two === undefined) {
+    //   two = "";
+    // } else if (three === undefined) {
+    //   three = "";
+    // } else if (four === undefined) {
+    //   four = "";
+    // }
+    // toppingsChecked.push([one, two, three, four])
+
   }
-  toppingsChecked.push([one, two, three, four])
-  return toppingsChecked;
-}
+
 
 
 $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("input:radio[name=pizzaSize]:checked").val();
+    var topping1 = checkIfUndefined($('input:checkbox[name=pepperoni]:checked').val());
+    // var topping1 = $('input:checkbox[name=pepperoni]:checked').val();
+    // var topping1Checked = checkIfUndefined(topping1);
 
-    var topping1 = $('input:checkbox[name=pepperoni]:checked').val();
-    var topping2 = $('input:checkbox[name=olives]:checked').val();
-    var topping3 = $('input:checkbox[name=mushroom]:checked').val();
-    var topping4 = $('input:checkbox[name=sausage]:checked').val();
-
-    var toppingsChecked = checkIfUndefined(topping1, topping2, topping3, topping4);
-    console.log(toppingsChecked);
+    var topping2 = checkIfUndefined($('input:checkbox[name=olives]:checked').val());
+    var topping3 = checkIfUndefined($('input:checkbox[name=mushroom]:checked').val());
+    var topping4 = checkIfUndefined($('input:checkbox[name=sausage]:checked').val());
+    console.log(topping1, topping2, topping3, topping4);
+    // var toppingsChecked = checkIfUndefined(topping1, topping2, topping3, topping4);
+    // console.log(toppingsChecked);
     customersPizza = new Pizza(pizzaSize);
-    toppingsOrdered = new Toppings(topping1, topping2, topping3, topping4)
-
+    toppingsOrdered = new Toppings(topping1, topping2, topping3, topping4);
+    // toppingsOrdered = new Toppings(topping1, topping2, topping3, topping4)
     customersPizza.toppings.push(toppingsOrdered);
+    // customersPizza.toppings.push(toppingsOrdered);
     // customersPizza.toppings.push(toppingsOrdered);
 
     console.log(customersPizza);
