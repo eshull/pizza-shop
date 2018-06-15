af80297bb9e9a0af7a16cb9451d1a969c1f3d47c
Eric Shull <eshulldesign@gmail.com>
2018-06-15 10:33:25 -0700

    values from checkboxes and put them into array

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,33 @@ var pizza = function Pizza(size, toppings) {
   this.toppings = toppings;
 }
 
-pizza.prototype.topping = function(){
-  this.toppings = toppingInput;
+pizza.prototype.topping = function(each){
+
+  this.toppings.forEach(function(each){
+    return each;
+  })
 }
+
+
 
 
 $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("input:radio[name=pizzaSize]:checked").val();
+    // toppings = document.getElementById("pepperoni").value;
+    var toppings = [];
+    toppings = $("input:checkbox[name=toppings]:checked").val();
+    allToppings = topping(toppings);
 
-    toppings = "pineapple"
+
+    // toppings = "pineapple"
 
     customersPizza = new pizza(pizzaSize, toppings);
     console.log(customersPizza);
+    allToppings = checkbox(toppings);
 
+    console.log(allToppings);
 
 
   });
