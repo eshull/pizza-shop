7b0da671ff24012bb9c0bb69dd7e438e83e88e20
Eric Shull <eshulldesign@gmail.com>
2018-06-15 08:46:14 -0700

    add object constructor, first prototype and basic UI jquery

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = []toppings;
+}
+
+pizza.prototype.topping() {
+  this.toppings = toppingInput;
+}
+
+
+$(document).ready(function() {
+  $("#orderForm").submit(function(event) {
+    event.preventDefault();
+
+    
+
+  });
+});
